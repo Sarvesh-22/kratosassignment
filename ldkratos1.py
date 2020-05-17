69aafdeadfd05f572e7ec7a01a56d9ea103a6d40
Sarvesh-22 <65503794+Sarvesh-22@users.noreply.github.com>
2020-05-17 23:20:57 +0530

    Update ldkratos1.py

--- ldkratos1.py
+++ ldkratos1.py
@@ -2,10 +2,12 @@ from PIL import Image
 import numpy
 im=Image.open(r'c:\Users\rajes\Pythoncode\.vscode\qstp.jpg')
 np_im=numpy.array(im)
+# inverting the image
 invert=255-np_im
 finalim=Image.fromarray(invert)
 finalim.show()
 finalim.save(r'c:\Users\rajes\Pythoncode\.vscode\finalqstp.jpg')
+# converting into grayscale image
 grayim=numpy.mean(np_im, axis=2)
 finalgrayim=Image.fromarray(grayim)
 finalgrayim=finalgrayim.convert("L")
